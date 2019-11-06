ed1bde1b31ee12e8d4c2d5d444948597521ecc42
Henrique Morbin <henrique.morbin@ifood.com.br>
2019-11-06 16:54:04 -0300

    Recomendation Header Cell

--- SwiftUIRecomendations/ContentView.swift
+++ SwiftUIRecomendations/ContentView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Recomendados para você")
+                .font(.subheadline)
+                .bold()
+            Text("Restaurantes escolhidos a partir das  suas respostas")
+                .font(.footnote)
+                .foregroundColor(.gray)
+                .fontWeight(.medium)
+        }
+        .padding(10)
+        .background(Color.white)
+        .clipped()
+        .cornerRadius(4)
+        .shadow(color: .shadow, radius: 1, x: 0, y: 0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension Color {
+    static var shadow: Color {
+        .init(Color.RGBColorSpace.sRGB, white: 0, opacity: 0.3)
     }
 }
