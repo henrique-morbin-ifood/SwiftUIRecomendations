a133351e9936bfc113b18df000cba81ea6f0e6a0
Henrique Morbin <henrique.morbin@ifood.com.br>
2019-11-06 16:12:43 -0300

    Initial Commit

---
+++ SwiftUIRecomendations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIRecomendations
+//
+//  Created by Henrique Morbin on 06/11/19.
+//  Copyright © 2019 Henrique Morbin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIRecomendationsTests/SwiftUIRecomendationsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIRecomendationsTests.swift
+//  SwiftUIRecomendationsTests
+//
+//  Created by Henrique Morbin on 06/11/19.
+//  Copyright © 2019 Henrique Morbin. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUIRecomendations
+
+class SwiftUIRecomendationsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
