ef670a9165a9d8cb1519eb9aa8edb5c47201f763
Henrique Morbin <henrique.morbin@ifood.com.br>
2019-11-08 08:44:27 -0300

    Recommendation Cell

--- SwiftUIRecomendations/ContentView.swift
+++ SwiftUIRecomendations/ContentView.swift
@@ -10,6 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+
+        HStack {
+            Image("mcdonalds")
+                .resizable()
+                .padding(12)
+                .frame(width: 48, height: 48, alignment: .center)
+                .background(Color.red)
+                .cornerRadius(24)
+            Divider()
+            VStack(alignment: .leading, spacing: 16) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Lorem ipsum")
+                        .font(.subheadline)
+                        .bold()
+                    HStack {
+                        Text("★ 5.0")
+                            .foregroundColor(Color.yellow)
+                            .font(.caption)
+                            .bold()
+                        Text("· Contemporânea · 1,9 km")
+                            .foregroundColor(.gray)
+                            .font(.caption)
+                    }
+                }
+                Text("35-45 min · R$ 9,90")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+            .padding(8)
+        }
+    }
+}
+
+struct HeaderCell: View {
+    var body: some View {
         VStack {
             Text("Recomendados para você")
                 .font(.subheadline)
